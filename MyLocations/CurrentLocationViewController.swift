3b3ebc97b1bb9b53144876c6d81f2460ce2b89ed
Nikita <ukr_vezunchik@ukr.net>
2018-01-05 17:26:15 +0200

    Get Location

--- MyLocations/CurrentLocationViewController.swift
+++ MyLocations/CurrentLocationViewController.swift
@@ -7,34 +7,116 @@
 //
 
 import UIKit
+import CoreLocation
 
-class CurrentLocationViewController: UIViewController {
+class CurrentLocationViewController: UIViewController, CLLocationManagerDelegate {
     
     // Outlets ==============================
-    @IBOutlet weak var massageLabel: UILabel!
+    @IBOutlet weak var messageLabel: UILabel!
     @IBOutlet weak var latitudeLabel: UILabel!
     @IBOutlet weak var longitudeLabel: UILabel!
     @IBOutlet weak var addressLabel: UILabel!
     @IBOutlet weak var tagButton: UIButton!
     @IBOutlet weak var getButton: UIButton!
+    // Variables ---
+    let locationManager = CLLocationManager() // The CLLocationManager is the object that will give you the GPS coordinates.
+    var location: CLLocation? // Will store the most recent update of user's location
     // =========================================
+    
+    // Override functions ======
+    override func viewDidLoad() {
+        // phase one
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view, typically from a nib.
+        updateLabels()
+        let authStatus = CLLocationManager.authorizationStatus()
+        // If authStatus did not determinate we set it (in the first run app only one time)
+        if authStatus == .notDetermined {
+            locationManager.requestWhenInUseAuthorization()
+        }
+        
+    }
+    // =========================
     
     // Actions =================
     @IBAction func getLocation() {
-        // do nothing yet
-        print("Hello")
+        // Now we already have a determinated status
+        let authStatus = CLLocationManager.authorizationStatus()
+        
+        // If authStatus do not allow to Get Locations
+        if authStatus == .denied || authStatus == .restricted {
+            showLocationServicesDeniedAlert()
+            return
+        }
+        
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBestForNavigation
+        locationManager.startUpdatingLocation()
     }
     // =========================
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    
+    // Functions ===============
+    // Alert for situation when we do not have enable to the location services
+    func showLocationServicesDeniedAlert() {
+        let alert = UIAlertController(title: "Location Services Disabled", message: "Please enable location services for this app in Settings.", preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+        present(alert,animated: true,completion: nil)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    // Update labels
+    func updateLabels() {
+        if let location = location {
+            latitudeLabel.text = String(format: "%.8f", location.coordinate.latitude)
+            longitudeLabel.text = String(format: "%.8f", location.coordinate.longitude)
+            tagButton.isHidden = false
+            messageLabel.text = ""
+        } else {
+            latitudeLabel.text = ""
+            longitudeLabel.text = ""
+            addressLabel.text = ""
+            tagButton.isHidden = true
+            messageLabel.text = "Tap 'Get My Location' to Start"
+        }
     }
-
+    // =========================
+    
+    // CLLocationManegerDelegate ===========
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print("didFailWithError \(error)")
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        location = locations.last!
+        print("didUpdateLocations \(location!)")
+        updateLabels()
+    }
+    // =====================================
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
